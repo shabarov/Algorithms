b399fa41c19be5940ad6837f916d50a417c3f269
Mikhail Shabarov <amigo141@rambler.ru>
2019-09-09 17:12:30 +0300

    Sqrt function

---
+++ src/main/java/ru/shabarov/common/misc/Misc.java
@@ -0,0 +1,24 @@
+package ru.shabarov.common.misc;
+
+public class Misc {
+
+    public static void main(String [] args) {
+        System.out.println("sqrt(16) = " + sqrt(16));
+    }
+
+    /**
+     * sqrt_n+1 = (sqrt_n + (num / sqrt_n)) / 2.0
+     *
+     * if a^2 = N
+     * then a = (a + N/a) / 2
+     */
+    private static double sqrt(int number) {
+        double t;
+        double squareRoot = number / 2.0;
+        do {
+            t = squareRoot;
+            squareRoot = (t + (number / t)) / 2;
+        } while ((t - squareRoot) != 0);
+        return squareRoot;
+    }
+}
